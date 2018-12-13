73f1c5d4ce58d89b3c6c20f2bf4db33ddcb100e3
Gatare Libère <gatarelib@gmail.com>
2018-12-14 00:54:37 +0300

    Create post.py

---
+++ post.py
@@ -0,0 +1,28 @@
+from models.database import Database
+
+__author__ = 'Libère'
+
+
+class Post(object):
+
+    def __init__(self, title, content, author, date, id):
+        self.blog_id = blog_id
+        self.title  = title
+        self.content = content
+        self.author = author
+        self.created_date = date
+        self.id = id
+
+    def save_to_mongo(self):
+        Database.insert(collection='post',
+                        data=self.json())
+
+    def json(self):
+        return {
+            'id' : self.id,
+            'blog_id' : self.blog_id,
+            'author' : self.author,
+            'content' : self.content,
+            'title' : self.title,
+            'created_date' : self.created_date
+        }
