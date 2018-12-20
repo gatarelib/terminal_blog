a45cf35e0e3dae6f36544a8e0fbf83fa193ff742
Gatare Libère <gatarelib@gmail.com>
2018-12-21 00:20:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from models.database import Database
-from models.post import Post
+from database import Database
+from models.blog import Blog
 
 __author__ = 'Libère'
 
 Database.initialize()
 
-post = Post(blog_id="123",
-            title="Another great post",
-            author="Libère")
+blog = Blog(author="Libere",
+            title="Sample Title",
+            description="SampleDescription")
 
-post.save_to_mongo()
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
