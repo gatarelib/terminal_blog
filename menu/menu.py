445086be9e15fe52a46287667b730b9ba2e446de
Gatare Libère <gatarelib@gmail.com>
2018-12-21 00:24:20 +0300

    Create menu.py

---
+++ menu/menu.py
@@ -0,0 +1,18 @@
+class Menu(object):
+    def __init__(self):
+        #Ask user for author name
+        #Check if they've already got an account
+        #if not, prompt them to create one
+        pass
+
+    def run_menu(self):
+        #User Read or Write blog?
+        #if read:
+            #list blogs in database
+            #allow user to pick one
+            #desplay posts
+        #if write
+            #check if user has blog
+            #if they do, prompt to write post
+            #if not, prompt to create new blog
+        pass
